b9dc26ca19af19149c17b0c4888ed24c590f4475
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-01-05 18:55:28 -0300

    Create GGX/GGGX.lua

---
+++ src/GGX/GGGX.lua
@@ -0,0 +1,23 @@
+*// What is P/Lua/Global (2026-2020)
+
+function onCreate()
+
+     var
+
+assert(smggx0)
+
+loadstring(smggx0)
+
+     if GGX has sleep.GGX
+        nil GGGX
+   return GGX
+
+if GGX = GGGX
+
+return inf
+
+return prod.Render.Camma = void< ggx
+ 
+    gggx = false
+
+return 0
